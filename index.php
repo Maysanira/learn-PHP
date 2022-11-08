fa463ffe128b1cae12fe84f95ed86ba4a22274cb
Maysanira <indahermerah@gmail.com>
2022-11-08 19:56:27 +0700

    Menambahkan tipe data string

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+   Halo semuanya!
+   
+   <?php 
+   //belajar php
+   /* berapa baris
+   komentar
+    */
+
+    //variabel pada php
+$x = "Sekolah Koding 123";
+
+
+        echo"<h1>Sekolah Koding</h1> ";
+        echo "Halo".$x."<br>";
+        echo "Member"; 
+        ?>
+</body>
+</html>
