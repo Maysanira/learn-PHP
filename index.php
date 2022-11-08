a61728048c734a902eab12ca5bfcb5b971dd6e3d
Maysanira <indahermerah@gmail.com>
2022-11-08 20:23:49 +0700

    Menambahkan tipe integer dan float

--- index.php
+++ index.php
@@ -16,12 +16,29 @@
     */
 
     //variabel pada php
-$x = "Sekolah Koding 123";
+$nama = "Sekolah Koding 123";
+$n123 ="text"; //tdk boleh langsung mulai angka
 
-
-        echo"<h1>Sekolah Koding</h1> ";
-        echo "Halo".$x."<br>";
+        echo"<h1>$nama</h1> ";
+        echo "Halo".$nama."<br>";
         echo "Member"; 
+        
+     //tipe data -string-
+     $nama = "SEKOLAH KODING";
+     $nama2 ='bermain kodin';
+     $nama3 = '1000';
+     $nama4 = '1000';
+     
+     echo $nama3. $nama4;
+     echo "<br>";
+
+    //tipe data angka/number
+    //1. integer 2.Float
+    $angka = 1000;
+    $angka2 = 100.123;
+
+    echo $angka + $angka2;
+        
         ?>
 </body>
 </html>
