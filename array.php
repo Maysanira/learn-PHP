7e36da3ed7d7521795ced6fb352d7bc3c341fc5d
Maysanira <indahermerah@gmail.com>
2022-11-09 11:22:49 +0700

    Belajar array

---
+++ array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+
+<h1>Array PHP</h1>
+
+<?php
+// Tipe Data ARRAY
+$kotak = array('anjing','kurakura','koala');
+$nama = ['endah','pambud','galuh'];
+
+//print_r($kotak);
+echo $nama[2];
+
+?>
+    
+</body>
+</html>
