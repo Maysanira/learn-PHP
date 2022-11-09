b6a420864d38cbcdb717783f69d12c01f3a2a6cd
Maysanira <indahermerah@gmail.com>
2022-11-09 14:31:41 +0700

    foreach untuk asosiatif array

--- looping.php
+++ looping.php
@@ -12,21 +12,26 @@
    // for(variabelawal(mulai) ;batas(syarat) ;perubahan)
    //++ artinya bertambah 1
    
-   $buah = ['anggur','semangka','durian','melon','salak'];
-   for($a=0; $a < 4; $a++)
-   {
-    echo "----------------";
-    echo $buah [$a];
-    echo "---------------<br>";
-   }
-   foreach($buah as $a){
-    echo "----------------";
-    echo $a;
-    echo "---------------<br>";
-   }
+//    $buah = ['anggur','semangka','durian','melon','salak'];
+//    for($a=0; $a < 4; $a++)
+//    {
+//     echo "----------------";
+//     echo $buah [$a];
+//     echo "---------------<br>";
+//    }
+//    foreach($buah as $a){
+//     echo "----------------";
+//     echo $a;
+//     echo "---------------<br>";
+//    }
    
-   
-   
+   $data = ['nama' => 'endah',
+            'umur'=>37,
+            'sifat'=> 'kesed'];
+            
+    foreach($data as $key=>$value){
+        echo $value."<br>";
+                   }
    ?> 
 </body>
 </html>
