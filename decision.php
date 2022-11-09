259e70b69c0da043bdf2b4f0858ff2d2830fcd9d
Maysanira <indahermerah@gmail.com>
2022-11-09 19:20:23 +0700

    menambah else if

---
+++ decision.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<h1>, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Logika di PHP</h1>
+
+<?php
+// ----tipe data boolean---
+// $hasil = true;
+// $hasil2 = false;
+
+// if dan else
+// == digunakan untuk menguji benar atau salah
+// - utnutk variabel
+
+$password = '123';
+
+if($password == '123'){
+    echo 'Anda berhasil masuk!';
+}else{
+    echo 'Gagal! passwordnya salah';
+}
+    
+
+
+
+?>
+</body>
+</html>
