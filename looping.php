784ca04faa5481eec22f5ab1f856e28c46aa3853
Maysanira <indahermerah@gmail.com>
2022-11-09 13:52:59 +0700

    menambah loop

---
+++ looping.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loop PHP</title>
+</head>
+<body>
+   <?php
+   //-------pengulangan---------
+   // for(variabelawal(mulai) ;batas(syarat) ;perubahan)
+   //++ artinya bertambah 1
+   
+   for($i=0; $i < 8; $i++)
+   {
+    echo "----------------";
+    echo "UpoDjiwo";
+    echo "---------------<br>";
+
+   }
+   
+   
+   
+   
+   ?> 
+</body>
+</html>
